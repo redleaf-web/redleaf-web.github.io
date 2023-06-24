d9f7469b9cc0358a6dff60e3cd4f794ebef0fad4
Asperaven <102703036+Asperaven@users.noreply.github.com>
2023-06-24 20:33:16 -0300

    Add files via upload
    Correção da funcionalidade javascript para a to-do-list.

--- To-do List/to-do-list.js
+++ To-do List/to-do-list.js
@@ -1,171 +1,96 @@
-var undo = false;
-var list_item_to_undo;
-var time_out;
+document.addEventListener("DOMContentLoaded", function() {
+  var todoList = document.getElementById("todoList");
+  var newItemInput = document.getElementById("newItemInput");
+  var addButton = document.getElementById("addButton");
 
-$(document).ready(function(){ 
-  
-  /*DELETE*/
-  $('body').on('click', '.fa-trash', function() {
-    
-    if($('.main ul li').hasClass("temp_deleted")){
-      $('.main ul li.temp_deleted').remove();
-    }
-    
-    list_item_to_undo = $(this).parent().parent();    
-    var undo_time = 10000;
-    
-    list_item_to_undo.animate({
-      height: "0px"
-    }, 200, function() {
-      $(this).addClass('temp_deleted').hide();
-    });
-    
-    function_undo(list_item_to_undo, undo);     
-   
-    //undo
-    $('.undo').addClass('active');
-    
-    clearTimeout(time_out);
-    
-    time_out = setTimeout(function() {
-      $('.undo').removeClass('active');      
-      if (undo === false){        
-        $('.main ul li.temp_deleted').remove();
-      }
-    }, undo_time);
-    
-  });
-  /*DELETE*/
-  
-  
-  /*UNDO*/
-  $('.undo div').click(function(){
-    undo = true;
-    function_undo(list_item_to_undo, undo);
-    $(this).parent().removeClass('active');
-  });
-  
-  
-  /*EDIT*/
-  $('body').on('click', '.fa-pencil', function() {
-     var current = $(this).parent().parent().find('span').text();
-     $(this).parent().parent().find('input[type=text]').val(current).show().select();
-  });
-  
-  
-  $('body').on('keypress', '.main ul li input[type=text]', function(e) {
-      if (e.which == 13) {
-        //hide undo
-        $('.undo').removeClass('active');
-        
-        var newvalue = $(this).val();
-        $(this).parent().parent().find('label span.item-name').text(newvalue);
-        $(this).hide();
-        return false;    //<---- Add this line
-      } 
-  });
-  
-  
-  $(document).on('blur', 'input[type=text]', function() {   
-    $(this).hide();
-  });
-  
-  
-  
-  
-  
-  /*ADD NEW*/
-   $('.add').click(function(){
-    $(this).find('input[type=text]').val('Add new').show().select();
-  });
-  
-  
-  $('body').on('keypress', '.add input[type=text]', function(e) {
-    if (e.which == 13) {
-      
-      //hide undo
-      $('.undo').removeClass('active');
-      
-      var newvalue = $(this).val();
-      
-      var clone = $(".main ul li:first").clone();
-      clone.find('label span.item-name').text(newvalue);
-      
-      var random_num = Math.floor(Math.random() * 1000) + 1 
-      var id = newvalue.replace(/\s/g,'');
-      var ids = id + random_num;
- 
-      clone.find('input').attr({
-          id:ids
-      });      
-     clone.find('label').attr('for', ids);
-     clone.find("input[type=checkbox]").prop('checked', false);  
-     clone.removeClass('priority priority1 priority2 checked');
-     clone.show();
-     clone.appendTo(".main ul");
-      
-      $( '.add' ).trigger( "click" );
-      return false;    //<---- Add this line
+  addButton.addEventListener("click", addItem);
+  newItemInput.addEventListener("keyup", function(event) {
+    if (event.keyCode === 13) {
+      addItem();
     }
   });
-  
-  
-  
-  /*PRIORITY*/  
-  $('body').on('click', '.main ul li .actions .prio-btn', function() {
-    
-    if($(this).parent().parent().hasClass("priority2")){
-        
-        $(this).parent().parent().addClass('reset').removeClass("priority priority1 priority2 ");
-      
-      return false;
+
+  function addItem() {
+    var newItemText = newItemInput.value.trim();
+    if (newItemText !== "") {
+      var newItem = createNewItem(newItemText);
+      todoList.appendChild(newItem);
+      newItemInput.value = "";
+      saveItems();
     }
-
-    if($(this).parent().parent().hasClass("priority1")){
-        $(this).parent().parent().addClass("priority2");
-    }
-     
-    $(this).parent().parent().addClass('priority priority1');
-         
-  });
-   
-  
-  /*CLICK*/
-  $('body').on('change', '.main ul li input[type=checkbox]', function() {
-    if ($(this).prop('checked')) {
-      $(this).parent().addClass('checked');
-    }else{
-      $(this).parent().removeClass('checked');
-    }
-    
-  });
-  
-  
-  /*SORTABLE*/
-  $( function() {
-    $( ".main ul" ).sortable({
-      animation: 100,
-      delay: 150, 
-      cursor:'move',
-      handle: ".dragger",
-      tolerance: "pointer",
-      axis:'y'
-    });
-    $( "#sortable" ).disableSelection();
-  } );
-  
-  
-
-  
-  
-  
-});
-
-
-function function_undo(list_item_to_undo, undo){  
-  if(undo==true){
-      list_item_to_undo.css('height', 'auto');
-      list_item_to_undo.show();
-      list_item_to_undo.removeClass('temp_deleted');
   }
-}
+
+  function createNewItem(itemText, isChecked) {
+    var listItem = document.createElement("li");
+    var itemCheckbox = document.createElement("input");
+    itemCheckbox.type = "checkbox";
+    itemCheckbox.checked = isChecked || false;
+
+    var editInput = document.createElement("input");
+    editInput.type = "text";
+    editInput.classList.add("edit-input");
+    editInput.value = itemText;
+
+    var deleteButton = document.createElement("button");
+    deleteButton.textContent = "Delete";
+    deleteButton.classList.add("delete-button");
+    deleteButton.addEventListener("click", function() {
+      listItem.remove();
+      saveItems();
+    });
+
+    listItem.appendChild(itemCheckbox);
+    listItem.appendChild(editInput);
+    listItem.appendChild(deleteButton);
+
+    itemCheckbox.addEventListener("change", function() {
+      listItem.classList.toggle("done", itemCheckbox.checked);
+      saveItems();
+    });
+
+    return listItem;
+  }
+
+  function toggleEditState(listItem, editInput) {
+    var isEditing = listItem.classList.toggle("editing");
+
+    if (isEditing) {
+      editInput.addEventListener("keyup", function(event) {
+        if (event.keyCode === 13) {
+          toggleEditState(listItem, editInput);
+          saveItems();
+        }
+      });
+      editInput.focus();
+    } else {
+      saveItems();
+    }
+  }
+
+  function saveItems() {
+    var items = [];
+    var listItems = todoList.getElementsByTagName("li");
+    for (var i = 0; i < listItems.length; i++) {
+      var listItem = listItems[i];
+      var itemCheckbox = listItem.getElementsByTagName("input")[0];
+      var itemText = listItem.getElementsByClassName("edit-input")[0].value.trim();
+      var isChecked = itemCheckbox.checked;
+      items.push({ text: itemText, checked: isChecked });
+    }
+    localStorage.setItem("todoItems", JSON.stringify(items));
+  }
+
+  function loadItems() {
+    var savedItems = localStorage.getItem("todoItems");
+    if (savedItems) {
+      var items = JSON.parse(savedItems);
+      for (var i = 0; i < items.length; i++) {
+        var item = items[i];
+        var newItem = createNewItem(item.text, item.checked);
+        todoList.appendChild(newItem);
+      }
+    }
+  }
+
+  loadItems();
+});
